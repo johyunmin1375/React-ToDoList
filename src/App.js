943eda8176b1568e76b907757056491881cadc90
johyunmin1375 <whgusals9260@gmail.com>
2021-12-03 09:01:17 +0900

    수정, 삭제 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useCallback } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoTemplate from './components/TodoTemplate';
 import TodoList  from './components/TodoList';
@@ -21,10 +21,49 @@ const App = () =>{
       checked : false,
     }
   ]);
+
+  //고유 값으로 사용될 id
+  // ref를 사용하여 변수 담기
+  const nexId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id : nexId.current,
+        text,
+        checked : false,
+      };
+      setTodos(todos.concat(todo));
+      nexId.current +=1; // nextId 1씩 더하기
+    },
+    [todos],
+  );
+
+  // todos 배열에서 id로 항목 지우기
+  const onRemove = useCallback(
+    id => {
+      setTodos(todos.filter(todo => todo.id !==id));
+    },
+    [todos],
+  );
+
+  // 수정 기능
+  const onToggle = useCallback(
+    id => {
+      setTodos(
+        todos.map(todo =>
+          // todo.id와 현재 파라미터로 사용된 id 값이 같을 때 처음 받아 왔던 상태 그래도 반환
+          todo.id === id ? { ...todo, checked: !todo.checked} : todo,
+          ),
+      );
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert/>
-      <TodoList todos ={todos}/>
+      <TodoInsert onInsert={onInsert}/>
+      <TodoList todos ={todos} onRemove={onRemove} onToggle={onToggle}/>
     </TodoTemplate>
   );
 }
